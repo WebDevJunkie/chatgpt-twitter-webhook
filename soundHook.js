193fc0f70c1e3327e36e4faf4742ee588b80724c
Mike Wittenauer <mikewittenauer@me.com>
2022-12-23 10:15:01 -0500

    Add soundHook for making sound on raspberry pi when tweet is retweeted

--- soundHook.js
+++ soundHook.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const { exec } = require('child_process');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,22 +11,12 @@ app.post('/webhook', (req, res) => {
 
   // Check if the retweet event is for one of your tweets
   if (retweetEvent.user.screen_name === process.env.TWITTER_HANDLE) {
-    // Send a text message to your phone
-    request.post(`https://api.twilio.com/2010-04-01/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Messages.json`, {
-      auth: {
-        user: process.env.TWILIO_ACCOUNT_SID,
-        pass: process.env.TWILIO_AUTH_TOKEN
-      },
-      form: {
-        From: process.env.TWILIO_PHONE_NUMBER,
-        To: process.env.PHONE_NUMBER,
-        Body: 'Someone just retweeted one of your tweets!'
-      }
-    }, (error, response, body) => {
+    // Play a sound file on your Raspberry Pi
+    exec('omxplayer /path/to/sound/file.mp3', (error, stdout, stderr) => {
       if (error) {
         console.error(error);
       } else {
-        console.log(response.statusCode, body);
+        console.log(stdout);
       }
     });
   }
@@ -38,5 +27,4 @@ app.post('/webhook', (req, res) => {
 app.listen(3000, () => {
   console.log('Webhook listening on port 3000');
 });
-
 
